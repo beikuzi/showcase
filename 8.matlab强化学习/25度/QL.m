7f3c662a60b6079c0f37c737abd1bf28059994f0
北枯紫 <1315349298@qq.com>
2023-07-02 14:49:04 +0800

    7.2

---
+++ 8.matlab强化学习/25度/QL.m
@@ -0,0 +1,57 @@
+rng(0)
+
+obsInfo = rlNumericSpec([4 1]);
+obsInfo.Name = "observations";
+
+actInfo = rlFiniteSetSpec([0,1]);
+actInfo.Name = "action";
+
+env = rlSimulinkEnv("study","study/RL Agent",...
+    obsInfo,actInfo);
+
+criticNet = [
+    featureInputLayer(obsInfo.Dimension(1))
+    fullyConnectedLayer(20)
+    reluLayer
+    fullyConnectedLayer(10)
+    sigmoidLayer
+    fullyConnectedLayer(length(actInfo.Elements))
+];
+
+%深度网络
+net = dlnetwork(criticNet);
+summary(net);
+plot(net);
+critic = rlVectorQValueFunction(net,obsInfo,actInfo);
+getValue(critic,{rand(obsInfo.Dimension)});
+
+%设置学习率
+criticOpts = rlOptimizerOptions(LearnRate=0.01);
+%设置agent参数
+Ts = 0.005;
+Tf = 30;
+agentOptions = rlDQNAgentOptions(...
+    SampleTime=Ts,...
+    CriticOptimizerOptions=criticOpts);
+agentOpts.UseDevice = 'gpu';
+%设置agent
+myAgent=rlDQNAgent(critic,agentOptions);
+%未知
+getAction(myAgent,{rand(obsInfo.Dimension)});
+
+%设置训练参数
+trainOpts = rlTrainingOptions;
+trainOpts.MaxStepsPerEpisode = 400;
+trainOpts.MaxEpisodes= 50;
+
+learning=true;
+if learning
+    trainingStats = train(myAgent,env,trainOpts);%强化学习
+    save('train.mat',"myAgent");
+else
+    load('train.mat',"myAgent");
+end
+
+
+%进行评估
+sim(myAgent,env)
---
+++ 8.matlab强化学习/使用随机温度训练/QL.m
@@ -0,0 +1,56 @@
+gpuDevice(1);
+rng(0)
+obsInfo = rlNumericSpec([4 1]);
+obsInfo.Name = "observations";
+
+actInfo = rlFiniteSetSpec([0,1]);
+actInfo.Name = "action";
+
+env = rlSimulinkEnv("study","study/RL Agent",...
+    obsInfo,actInfo);
+
+criticNet = [
+    featureInputLayer(obsInfo.Dimension(1))
+    fullyConnectedLayer(20)
+    reluLayer
+    fullyConnectedLayer(20)
+    sigmoidLayer
+    fullyConnectedLayer(length(actInfo.Elements))
+];
+
+%深度网络
+net = dlnetwork(criticNet);
+summary(net);
+%plot(net);
+critic = rlVectorQValueFunction(net,obsInfo,actInfo);
+getValue(critic,{rand(obsInfo.Dimension)});
+
+%设置学习率
+criticOpts = rlOptimizerOptions(LearnRate=0.01);
+%设置agent参数
+Ts = 0.005;
+agentOptions = rlDQNAgentOptions(...
+    SampleTime=Ts,...
+    CriticOptimizerOptions=criticOpts);
+%设置agent
+myAgent=rlDQNAgent(critic,agentOptions);
+%未知
+getAction(myAgent,{rand(obsInfo.Dimension)});
+
+%设置训练参数
+trainOpts = rlTrainingOptions;
+trainOpts.MaxStepsPerEpisode = 400;
+trainOpts.MaxEpisodes=100;
+
+
+learning=false;
+if learning
+    trainingStats = train(myAgent,env,trainOpts);%强化学习
+    %save('train2.mat',"myAgent");
+else
+    load('train2.mat',"myAgent");
+end
+
+
+%进行评估
+sim(myAgent,env)
